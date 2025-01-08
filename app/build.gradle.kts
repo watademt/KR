1606358ebbe9ffe1d777c9928e0abb68af51e44d
StepaKUL <kulik-stepa@mail.ru>
2025-01-08 22:58:43 +0500

    gok
    Добавил библиотеки для работы с firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.gms)
 }
 
 android {
@@ -50,7 +51,8 @@ android {
 }
 
 dependencies {
-    implementation (libs.firebase.firestore)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.firestore)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
